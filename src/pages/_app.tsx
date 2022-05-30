6e789c3a68e4db15a4f8261db29d4edc93678ab5
frankhn <harfrank2@gmail.com>
2022-05-30 10:45:25 +0200

    initial app config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const [queryClient] = useState(() => new QueryClient());
+  return <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+}
+
+export default MyApp
