60e17d0ca8f10bfccb5c6190b6989e607550b678
frankhn <harfrank2@gmail.com>
2022-06-01 21:47:07 +0200

    adds text component

---
+++ src/components/common/TextInput/index.ts
@@ -0,0 +1,5 @@
+import BaseInput from './Base';
+export { BaseInput as Base } from './Base';
+export { PasswordInput as Password } from './Password';;
+export { DateInput as Date } from './DateInput';
+export default BaseInput;
